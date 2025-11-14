503e726709a0a771e2e21e2c7d3b92ae51a92f3a
吴健ZIY00178659 <149761789@qq.com>
2025-11-14 10:37:03 +0800

    feat: 增加测试页描述

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -3,14 +3,19 @@
  * @Author: JZT.吴健
  * @Date: 2025-04-20 21:57:02
  * @LastEditors: JZT.吴健
- * @LastEditTime: 2025-11-14 08:38:22
+ * @LastEditTime: 2025-11-14 10:36:17
 -->
 <template>
     <div>子应用 Page1</div>
     <form>
-        <input v-model="name" type="text" placeholder="请输入用户名" />
+        这是form内的input: <input v-model="name" type="text" placeholder="请输入用户名" /> 输入后，点击下方跳转page2 edge下页面崩溃
     </form>
-    <button @click="goPage2">page2</button>
+    <div style="margin-top: 20px;">
+        这是form外的普通input
+        <input v-model="name2" type="text" placeholder="请输入用户名" />
+        输入后，点击下方跳转page2 页面正常
+    </div>
+    <button style="margin-top: 20px;" @click="goPage2">跳转page2</button>
 </template>
 
 <script setup>
@@ -18,6 +23,7 @@
     const router = useRouter()
     import { ref } from 'vue'
     const name = ref('')
+    const name2 = ref('')
     function goPage2() {
         router.push({
             path: '/page2'
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -3,10 +3,19 @@
  * @Author: JZT.吴健
  * @Date: 2025-04-20 21:57:02
  * @LastEditors: JZT.吴健
- * @LastEditTime: 2025-11-13 18:32:30
+ * @LastEditTime: 2025-11-14 10:32:53
 -->
 <template>
     <div>子应用 page2</div>
+    <button @click="goPage1">返回page1</button>
 </template>
 
-<script setup></script>
+<script setup>
+    import { useRouter } from 'vue-router'
+    const router = useRouter()
+    function goPage1() {
+        router.replace({
+            path: '/'
+        })
+    }
+</script>
